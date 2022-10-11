dd0ce5e8c56ade47f3832373bfaefb5a253b0d4e
RhoSeungA <sskclara@naver.com>
2022-10-11 17:37:31 +0900

    포스트 목록/상세페이지에 첨부 파일, 미리보기, 필터 작성하기

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,12 @@
 from django.db import models
-
+import os
 # Create your models here.
 
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
+    hook_text = models.CharField(max_length=100, blank=True)
+
     content = models.TextField()
 
     # %Y 2022, %y 22
@@ -25,3 +27,11 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
+        #basename은 파일이름만
+    def get_file_ext(self):
+        return self.get_file_name().split('.')[-1] #~.txt 나 ~.xlsx 여기서 확장자만 분리해야함. split으로 분리
+        #a.txt이면,,, [0]은 a이고, [1]은 txt
+        #근데 문제는 a.b.x.txt 같은 형태고 있음, 따라서 -1로 해주는 것이 좋음.
